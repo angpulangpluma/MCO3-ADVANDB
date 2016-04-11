d658223e101a7bae6a45470233175e201a408093
angpulangpluma <mariennelopez30@gmail.com>
2016-04-11 16:56:03 +0800

    mco3 taking shape

---
+++ form.php
@@ -0,0 +1,89 @@
+
+<html>
+ <body>
+  <style>
+   html {<!--overflow-y:hidden; overflow-x:hidden;-->}
+   body {font-family:verdana; background-image:url("pic/bg3.jpg");}
+   #top {position:fixed; top:50%; left:50%; margin-top:-272; margin-left:-272; color:black; z-index:10}
+   #content {position:absolute; border-radius:8px; width:1200px; top:50%; left:50%; margin-top:-300px; margin-left:-600px; text-align:center; clear:both; z-index:5}
+   #end {position:absolute; text-align:center; padding-top:5px; padding-bottom:5px; color:white; width:100%; height:20px; bottom:0; left:0; background:rgba(192,192,192,0.7);}
+   .rainbow {
+    background-image: -webkit-gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );
+    background-image: gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );
+    color:transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+   }
+  </style>
+  <title>
+   GUTEN TAG!
+  </title>
+  <?php
+   session_start();
+   
+   //Marinduque Information
+   if (isset($_POST["mar"])) $_SESSION["mar"] = "y";
+   if (isset($_POST["mar"]) && $_POST["im"] != "") $_SESSION["im"] = $_POST["im"];
+   else $_SESSION["mar"] = "n";
+   if (isset($_POST["mar"]) && $_POST["um"] != "") $_SESSION["um"] = $_POST["um"];
+   else $_SESSION["mar"] = "n";
+   if (isset($_POST["mar"])) $_SESSION["pm"] = $_POST["pm"];
+   
+   //Palawan Information
+   if (isset($_POST["pal"])) $_SESSION["pal"] = "y";
+   if (isset($_POST["pal"]) && $_POST["ip"] != "") $_SESSION["ip"] = $_POST["ip"];
+   else $_SESSION["pal"] = "n";
+   if (isset($_POST["pal"]) && $_POST["up"] != "") $_SESSION["up"] = $_POST["up"];
+   else $_SESSION["pal"] = "n";
+   if (isset($_POST["pal"])) $_SESSION["pp"] = $_POST["pp"];
+   
+   //Combined Information
+   if (isset($_POST["com"])) $_SESSION["com"] = "y";
+   if (isset($_POST["com"]) && $_POST["ic"] != "") $_SESSION["ic"] = $_POST["ic"];
+   else $_SESSION["com"] = "n";
+   if (isset($_POST["com"]) && $_POST["uc"] != "") $_SESSION["uc"] = $_POST["uc"];
+   else $_SESSION["com"] = "n";
+   if (isset($_POST["com"])) $_SESSION["pc"] = $_POST["pc"];
+  ?>
+  <div id="content" style="border:1px solid; background:white;">
+   <p style="padding-top:25px;">
+   <img src="pic/gt.png">
+   <p style="padding-top:25px; padding-right:0px;">
+   <b>!GAT NETUG</b></p>
+   <form name="honoka" action="results3.php" method="post">
+   		<p style="padding-left:10px; padding-right:10px; text-align:left;">
+	    <b>Edit Member:</b></p>
+	    <p style="padding-left:30px; padding-right:10px; text-align:left;">
+   </form>
+   <!-- <form name="mahjong" action="results2.php" method="post">
+    <p style="padding-left:10px; padding-right:10px; text-align:left;">
+    <b>Queries:</b></p>
+    <p style="padding-left:30px; padding-right:10px; text-align:left;">
+    <input style="padding-left:5px; height:25px;" name="queries" type="submit" value="Submit Query 1">    <b>1.</b> Number of community members that are registered voters but are not studying and are unemployed <br><br>
+    <input style="padding-left:5px; height:25px;" name="queries" type="submit" value="Submit Query 2">    <b>2.</b> Number of community members that are working students and are minor <br><br>
+    <input style="padding-left:5px; height:25px;" name="queries" type="submit" value="Submit Query 3">    <b>3.</b> Number of deaths per municipality that were caused by common diseases <br><br>
+    Show by...
+    <select name="query3">
+     <option value='m'>Municipality</option>
+     <option value='b'>Barangay</option>
+     <option value='p'>Purok</option>
+    </select>
+    <br><br>
+    <input style="padding-left:5px; height:25px;" name="queries" type="submit" value="Submit Query 4">     <b>4.</b> Crimes that are being done to people per municipality <br><br>
+    Show by...
+    <select name="query4">
+     <option value='m'>Municipality</option>
+     <option value='b'>Barangay</option>
+     <option value='p'>Purok</option>
+    </select>
+    <br><br>
+    <input style="padding-left:5px; height:25px;" name="queries" type="submit" value="Submit Query 5">  <b>5.</b> Average number of crops harvested by people who work in the crop industry per municipality for the last 12 months <br><br>
+    <input style="adding-left:5px; height:25px;" name="queries" type="submit" value="Submit Query 6">  <b>6.</b> Average number of seafood fished from the sea per municipality from the people in the crop industry for the last 12 months <br><br>
+    <input style="padding-left:5px; height:25px;" name="queries" type="submit" value="Submit Query 7">  <b>7.</b> Income of the fishing, crop, and livestock industry per municipality with the average yield of crops and fish for the last 12 months <br><br>
+     </p> -->
+    
+    <!--<a style="color:gray;" href="terms.html"><i>Don't have an account? Click here!</i></a>-->
+   </form>
+  </div>
+ </body>
+</html>
---
+++ results2.php
@@ -0,0 +1,71 @@
+
+<html>
+ <head>
+  <title>
+   Query Results
+  </title>
+ </head>
+ <body>
+  <style>
+   html {overflow-y:hidden; overflow-x:hidden;}
+   body {font-family:verdana; background-image:url("pic/bg3.jpg");}
+   #top {position:fixed; top:50%; left:50%; margin-top:-272; margin-left:-272; color:black; z-index:10}
+   #content {position:absolute; border-radius:8px; width:1200px; top:50%; left:50%; margin-top:-300px; margin-left:-600px; text-align:center; clear:both; z-index:5}
+   #end {position:absolute; text-align:center; padding-top:5px; padding-bottom:5px; color:white; width:100%; height:20px; bottom:0; left:0; background:rgba(192,192,192,0.7);}
+   .rainbow {
+    background-image: -webkit-gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );
+    background-image: gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );
+    color:transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+   }
+  </style>
+  <center>
+  <div id="content" style="border:1px solid; background:white;">
+   <?php
+    //Don't forget to change the table!
+    session_start();
+    
+    if ($_SESSION["mar"] == 'y') $mar = mysqli_connect($_SESSION["im"],$_SESSION["um"],$_SESSION["pm"],"marinduque_info") 
+      or die("Unable to connect to Marinduque Database");
+    if ($_SESSION["pal"] == 'y') $pal = mysqli_connect($_SESSION["ip"],$_SESSION["up"],$_SESSION["pp"],"palawan_info") or die("Unable to connect to Palawan Database");
+      print_r($_SESSION);
+    if ($_SESSION["com"] == 'y') $com = mysqli_connect($_SESSION["ic"],$_SESSION["uc"],$_SESSION["pc"],"combined",true) or die("Unable to connect to Combined Database");
+    
+    //Different queries
+    if ($_POST['queries'] == 'Submit Query 1') {
+     $sql = "SELECT COUNT(DISTINCT `main.id`) FROM hpq_mem
+       WHERE educind = 2 AND jobind = 2 AND regvotind = 1;";
+    }
+    
+    //Execution time in milliseconds
+    $exec = microtime(true);
+    mysqli_query($con, $sql);
+    $exec = microtime(true)-$exec;
+    echo 'Execution Time: ' . ($exec * 1000) . ' ms';
+    
+    $result = mysqli_query($con, $sql) or die(mysqli_error());
+    
+    //Print the column names
+    echo '<table>';
+    echo '<tr>';
+    while ($field_info = mysqli_fetch_field($result)) {
+     echo '<td>' . $field_info->name . '</td>';
+    }
+    echo '</tr>';
+    
+    //Print the data
+    while($row = mysqli_fetch_row($result)) {
+     echo "<tr>";
+     foreach($row as $value) {
+      echo "<td>" . $value . "</td>";
+     }
+     echo "</tr>";
+    }
+    echo '</table>';
+    mysqli_close($con);
+   ?>
+  </div>
+  </center>
+ </body>
+</html>
