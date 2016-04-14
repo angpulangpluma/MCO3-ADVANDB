230d6324c22c3dfe625e47e5362e515b0e849052
angpulangpluma <mariennelopez30@gmail.com>
2016-04-14 08:52:31 +0800

    so much stuff....
    - detect through mysqli_ping() when executing query
    - detect mysqli_connect()
    - detect valid or invalid IP address

--- results2.php
+++ results2.php
@@ -27,30 +27,64 @@
     //Don't forget to change the table!
     session_start();
 
-    $paldetect = false;
-    $mardetect = false;
-    $comdetect = false;
-    //detect which user is initiating transaction    
-    if ($_SESSION["mar"] == 'y'){
-      $mar = mysqli_connect($_SESSION["im"],$_SESSION["um"],$_SESSION["pm"],"marinduque_info");
-      $mardetect = true;
-      $con1 = $mar;
-    } else if ($_SESSION["pal"] == 'y'){
-      $pal = mysqli_connect($_SESSION["ip"],$_SESSION["up"],$_SESSION["pp"],"palawan_info");
-       $paldetect = true;
-       $con1 = $pal;
-    } else if ($_SESSION["com"] == 'y'){
-      $com = mysqli_connect($_SESSION["ic"],$_SESSION["uc"],$_SESSION["pc"],"combined");
-      $comdetect = true;
-      if($paldetect===true || $mardetect===true)
-        $con2 = $com;
-      else {
-        $existcon = false;
-        $con1 = $com; 
+    try{
+      $paldetect = false;
+      $mardetect = false;
+      $comdetect = false;
+      //detect which user is initiating transaction    
+      if ($_SESSION["mar"] == 'y'){
+        if(filter_var($_SESSION["im"], FILTER_VALIDATE_IP)===false && $_SESSION['im']!='localhost'){
+          throw new Exception("invalid ip addresss for marinduque connection");
+        } else{
+          $mar = mysqli_connect($_SESSION["im"],$_SESSION["um"],$_SESSION["pm"],"marinduque_info");
+          $mardetect = true;
+          $con1 = $mar;
+        }
+      } if ($_SESSION["pal"] == 'y'){
+        if(filter_var($_SESSION["ip"], FILTER_VALIDATE_IP)===false && $_SESSION['ip']!='localhost'){
+          throw new Exception("invalid ip addresss for palawan connection");
+        } else{
+          $pal = mysqli_connect($_SESSION["ip"],$_SESSION["up"],$_SESSION["pp"],"palawan_info");
+           $paldetect = true;
+           if($_SESSION["mar"]=='y'){
+            $con2 = $pal;
+           } else $con1 = $pal;
+        }
+      } if ($_SESSION["com"] == 'y'){
+        if(filter_var($_SESSION["ic"], FILTER_VALIDATE_IP)===false && $_SESSION['ic']!='localhost'){
+          throw new Exception("invalid ip addresss for combined connection");
+        } else{
+          $com = mysqli_connect($_SESSION["ic"],$_SESSION["uc"],$_SESSION["pc"],"combined");
+          $comdetect = true;
+          if($paldetect===true && $mardetect===true){
+            $con3 = $com;
+          } else if (($paldetect===true && $mardetect===false) || 
+              ($paldetect===false && $mardetect===true)){
+            $con2 = $com;
+          }
+          else {
+            // $existcon = false;
+            $con1 = $com; 
+          }
+        }
       }
-    }
 
-    switch($_POST['isol']){
+      //check connections
+      if($paldetect===false && $mardetect===true && 
+        $comdetect===true && ($con1===false || $con2===false)){ //for marinduque user
+        throw new Exception("cannot connect");
+      } else if ($paldetect===true && $mardetect===false && 
+        $comdetect===true && ($con1===false || $con2===false)){ //for palawan user
+        throw new Exception("cannot connect");
+      } else if ($paldetect===true && $mardetect===true &&
+        $comdetect===true && 
+        ($con1===false || $con2===false || $con3===false)){ //for combined user
+        throw new Exception("cannot connect");
+      } else if($con1===false){//one database only
+        throw new Exception("cannot connect");
+      }
+
+      switch($_POST['isol']){
       case 's': $isolevel = "SET TRANSACTION LEVEL SERIALIZABLE"; break;
       case 'rr': $isolevel = "SET TRANSACTION LEVEL REPEATABLE READ"; break;
       case 'rc': $isolevel = "SET TRANSACTION LEVEL READ COMMITTED"; break;
@@ -63,34 +97,41 @@
          $sql = "v";
          $sql1 = "SELECT COUNT(DISTINCT `main.id`) FROM hpq_mem
                 WHERE educind = 2 AND jobind = 2 AND regvotind = 1;";
-         if(($paldetect===true || $mardetect===true)&&$comdetect===true)
-            $sql2 = "SELECT COUNT(DISTINCT `main.id`) FROM hpq_mem
-                WHERE educind = 2 AND jobind = 2 AND regvotind = 1;";
+         // if (($paldetect===true && $mardetect===false) || 
+         //  ($paldetect===false && $mardetect===true) && $comdetect===true){
+         //  $sql2 = "SELECT COUNT(DISTINCT `main.id`) FROM hpq_mem
+         //        WHERE educind = 2 AND jobind = 2 AND regvotind = 1;";
+         // } else if ($paldetect===true && $mardetect===true && $comdetect===true){
+         //  $sql2 = "SELECT COUNT(DISTINCT `main.id`) FROM hpq_mem
+         //        WHERE educind = 2 AND jobind = 2 AND regvotind = 1;";
+         // }
          
       }else if( isset($_POST['edit']) && $_POST['edit'] == 'Edit User Info 1' && $_SESSION['mar']=='y' && isset($_POST['v1'])){ //cm1 - edit eduind at marinduque
         $sql = "cm1";
         $sql1 = "UPDATE hpq_mem SET educind =".$_POST['v1']." WHERE hpq_mem.`main.id`=199036 AND hpq_mem.`memno`=1";
-         if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+         if ($paldetect===false && $mardetect===true && $comdetect===true){
             $sql2 = "UPDATE hpq_mem SET educind =".$_POST['v1']." WHERE hpq_mem.`main.id`=199036 AND hpq_mem.`memno`=1";
+          }
          
       } else if( isset($_POST['edit']) && $_POST['edit'] == 'Edit User Info 1' && $_SESSION['pal']=='y' && isset($_POST['v1'])){ //cp1 - edit eduind at palawan
         $sql = "cp1";
         $sql1 = "UPDATE hpq_mem SET educind = ".$_POST['v1']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
-         if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+         if ($paldetect===true && $mardetect===false && $comdetect===true){
             $sql2 = "UPDATE hpq_mem SET educind = ".$_POST['v1']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
+        }
          
       } else if (isset($_POST['edit']) && $_POST['edit'] == 'Edit User Info 1' && isset($_POST['v1']) && $_SESSION['com']=='y' && isset($_POST['o1'])){//edit at combined
         switch($_POST['o1']){
           case '1':{
             $sql = "cm1";
             $sql1 = "UPDATE hpq_mem SET educind =".$_POST['v1']." WHERE hpq_mem.`main.id`=199036 AND hpq_mem.`memno`=1";
-             if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+             if ($paldetect===false && $mardetect===true && $comdetect===true)
                 $sql2 = "UPDATE hpq_mem SET educind =".$_POST['v1']." WHERE hpq_mem.`main.id`=199036 AND hpq_mem.`memno`=1";
           }; break;
           case '2':{
             $sql = "cp1";
             $sql1 = "UPDATE hpq_mem SET educind = ".$_POST['v1']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
-             if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+             if ($paldetect===true && $mardetect===false && $comdetect===true)
                 $sql2 = "UPDATE hpq_mem SET educind = ".$_POST['v1']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
           }; break;
         }
@@ -103,7 +144,7 @@
       }  else if( isset($_POST['edit']) && $_POST['edit'] == 'Edit User Info 2' && $_SESSION['pal']=='y' && isset($_POST['v2'])){  //cp2 - edit jobind at palawan
         $sql ="cp2";
         $sql1 = "UPDATE hpq_mem SET jobind = ".$_POST['v2']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
-         if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+         if ($paldetect===false && $mardetect===true && $comdetect===true)
             $sql2 = "UPDATE hpq_mem SET jobind = ".$_POST['v2']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
          
       }  else if (isset($_POST['edit']) && $_POST['edit'] == 'Edit User Info 2' && isset($_POST['v2']) && $_SESSION['com']=='y' && isset($_POST['o2'])){//edit at combined
@@ -111,26 +152,26 @@
           case '1':{
             $sql ="cm2";
             $sql1 = "UPDATE hpq_mem SET jobind =".$_POST['v2']." WHERE hpq_mem.`main.id`=199036 AND hpq_mem.`memno`=1";
-             if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+             if ($paldetect===false && $mardetect===true && $comdetect===true)
                 $sql2 = "UPDATE hpq_mem SET jobind =".$_POST['v2']." WHERE hpq_mem.`main.id`=199036 AND hpq_mem.`memno`=1";
           }; break;
           case '2':{
             $sql ="cp2";
             $sql1 = "UPDATE hpq_mem SET jobind = ".$_POST['v2']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
-             if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+             if ($paldetect===true && $mardetect===false && $comdetect===true)
                 $sql2 = "UPDATE hpq_mem SET jobind = ".$_POST['v2']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
           }; break;
         }
       }else if( isset($_POST['edit']) && $_POST['edit'] == 'Edit User Info 3' && $_SESSION['mar']=='y' && isset($_POST['v3'])){ //cm3 - edit regvotind at marinduque
         $sql ="cm3";
         $sql1 = "UPDATE hpq_mem SET regvotind =".$_POST['v3']." WHERE hpq_mem.`main.id`=199036 AND hpq_mem.`memno`=1";
-         if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+         if ($paldetect===false && $mardetect===true && $comdetect===true)
             $sql2 = "UPDATE hpq_mem SET regvotind =".$_POST['v3']." WHERE hpq_mem.`main.id`=199036 AND hpq_mem.`memno`=1";
          
       }  else if( isset($_POST['edit']) && $_POST['edit'] == 'Edit User Info 3' && $_SESSION['pal']=='y' && isset($_POST['v3'])){ //cp3 - edit regvotind at palawan
         $sql ="cp3";
         $sql1 = "UPDATE hpq_mem SET regvotind = ".$_POST['v3']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
-         if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+         if ($paldetect===true && $mardetect===false && $comdetect===true)
             $sql2 = "UPDATE hpq_mem SET regvotind = ".$_POST['v3']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
          
       } else if (isset($_POST['edit']) && $_POST['edit'] == 'Edit User Info 3' && isset($_POST['v3']) && $_SESSION['com']=='y' && isset($_POST['o3'])){//edit at combined
@@ -138,47 +179,141 @@
           case '1':{
             $sql ="cm3";
             $sql1 = "UPDATE hpq_mem SET regvotind =".$_POST['v3']." WHERE hpq_mem.`main.id`=199036 AND hpq_mem.`memno`=1";
-             if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+             if ($paldetect===false && $mardetect===true && $comdetect===true)
                 $sql2 = "UPDATE hpq_mem SET regvotind =".$_POST['v3']." WHERE hpq_mem.`main.id`=199036 AND hpq_mem.`memno`=1";
           }; break;
           case '2':{
             $sql ="cp3";
             $sql1 = "UPDATE hpq_mem SET regvotind = ".$_POST['v3']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
-             if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+             if ($paldetect===true && $mardetect===false && $comdetect===true)
                 $sql2 = "UPDATE hpq_mem SET regvotind = ".$_POST['v3']." WHERE hpq_mem.`main.id`=69279 AND hpq_mem.`memno`=15";
           }; break;
         }
       }
 
-      // print_r($_SESSION);
+      if($sql!=='v'){
+        mysqli_autocommit($con1, TRUE);
+        if($comdetect===true && $sql!=='v')
+          mysqli_autocommit($com3, TRUE);
+       //  if(($paldetect===true || $mardetect===true)&&$comdetect===true && $sql!=='v')
+        // mysqli_autocommit($con2, TRUE);
+      }
 
-      try{
-          //read write, if you want to read only replace it with READ_ONLY
-          /* if($sql!=='v'){
-            mysqli_begin_transaction($con1, MYSQLI_TRANS_START_READ_WRITE);
-            if(($paldetect===true || $mardetect===true)&&$comdetect===true && $sql!=='v')
-              mysqli_begin_transaction($con2, MYSQLI_TRANS_START_READ_WRITE);
-          } */
-		  
-		  if($sql!=='v'){
-			  mysqli_autocommit($con1, TRUE);
-			  if(($paldetect===true || $mardetect===true)&&$comdetect===true && $sql!=='v')
-				mysqli_autocommit($con2, TRUE);
-		  }
-
+          $editpal = false;
+          $editmar = false;
           if($_POST['isol']!='n'){
-            $db1query = mysqli_query($con1, $isolevel);
-            if(($paldetect===true || $mardetect===true)&&$comdetect===true)
-              $db2query = mysqli_query($con2, $isolevel);
-          else ;
-          } 
+            if($paldetect===true && $mardetect===true && $comdetect===true){
+              if(mysqli_ping($con1))
+                $db1query = mysqli_query($con1, $isolevel); //marinduque
+              else {
+                throw new Exception("query has not executed perfectly");
+              }
+              if(mysqli_ping($con2))
+                $db2query = mysqli_query($con2, $isolevel); //palawan
+              else{
+                throw new Exception("query has not executed perfectly");
+              }
+              if(mysqli_ping($con3))
+                $db3query = mysqli_query($con3, $isolevel); //combined
+              else{
+                throw new Exception("query has not executed perfectly");
+              }
+            } else if (($paldetect===false && $mardetect===true) ||
+                 ($paldetect===true && $mardetect===false)
+                 && $comdetect===true){
+              if(mysqli_ping($con1))
+                $db1query = mysqli_query($con1, $isolevel); //marinduque or palawan
+              else{
+                throw new Exception("query has not executed perfectly");
+              }
+              if(mysqli_ping($con2))
+                $db2query = mysqli_query($con2, $isolevel); //combined
+              else{
+                throw new Exception("query has not executed perfectly");
+              }
+              } else {
+                if(mysqli_ping($con1))
+                  $db1query = mysqli_query($con1, $isolevel); //mar or pal or com
+                else
+                  throw new Exception("query has not executed perfectly");
+            }
+            // if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+            //   $db2query = mysqli_query($con2, $isolevel);
+          } else ;
 
-          $db1query = mysqli_query($con1,$sql1);
-          if(($paldetect===true || $mardetect===true)&&$comdetect===true)
-            $db1query = mysqli_query($con2, $sql2);
-          else ;
+          if(isset($_POST['edit']) && $_POST['edit'] == 'Edit User Info 1' &&
+            $_SESSION['com']=='y'){
+              switch($_POST['o1']){
+                case '1':{//marinduque
+                  $db1query = mysqli_query($con1, $sql1);
+                  $db2query = mysqli_query($con3, $sql2);
+                  $editmar = true;
+                }; break;
+                case '2':{//palawan
+                  $db1query = mysqli_query($con2, $sql1);
+                  $db2query = mysqli_query($con3, $sql2);
+                  $editpal = true;
+                }; break; 
+              }
+          } else if(isset($_POST['edit']) && $_POST['edit'] == 'Edit User Info 2' &&
+            $_SESSION['com']=='y'){
+              switch($_POST['o2']){
+                case '1':{//marinduque
+                  $db1query = mysqli_query($con1, $sql1);
+                  $db2query = mysqli_query($con3, $sql2);
+                  $editmar = true;
+                }; break;
+                case '2':{//palawan
+                  $db1query = mysqli_query($con2, $sql1);
+                  $db2query = mysqli_query($con3, $sql2);
+                  $editpal = true;
+                }; break; 
+              }
+          } else if(isset($_POST['edit']) && $_POST['edit'] == 'Edit User Info 3' &&
+            $_SESSION['com']=='y'){
+              switch($_POST['o3']){
+                case '1':{//marinduque
+                  $db1query = mysqli_query($con1, $sql1);
+                  $db2query = mysqli_query($con3, $sql2);
+                  $editmar = true;
+                }; break;
+                case '2':{//palawan
+                  $db1query = mysqli_query($con2, $sql1);
+                  $db2query = mysqli_query($con3, $sql2);
+                  $editpal = true;
+                }; break; 
+              }
+          }else if($paldetect===false && $mardetect===true
+                 && $comdetect===true){
+            $db1query = mysqli_query($con1, $sql1);
+            $db2query = mysqli_query($con3, $sql2);
+            $editmar = true;
+          }else if($paldetect===true && $mardetect===false
+                 && $comdetect===true){
+            $db1query = mysqli_query($con1, $sql1);
+            $db2query = mysqli_query($con3, $sql2);
+            $editpal = true;
+          } else if(isset($_POST['queries']) && $_POST['queries']=='Submit Query 1' && ($paldetect===false &&
+                 $mardetect===true &&
+                  $comdetect===true)||
+                ($paldetect===true &&
+                  $mardetect===false &&
+                   $comdetect===true)){
+              $db1query = mysqli_query($con1, $sql1);
+              $db2query = mysqli_query($con2, $sql1);
+            
+          } else if(isset($_POST['queries']) && $_POST['queries']=='Submit Query 1' && $paldetect===true && $mardetect===true
+                 && $comdetect===true){
+              $db1query = mysqli_query($con3, $sql1);
+              $db2query = mysqli_query($con3, $sql1);
+          }
 
-          if ($sql==='v'){
+          // $db1query = mysqli_query($con1,$sql1);
+          // if(($paldetect===true || $mardetect===true)&&$comdetect===true)
+          //   $db1query = mysqli_query($con2, $sql2);
+          // else ;
+
+          if ($sql==='v' && $db1query===true && $db2query===true){
             //Print the column names
             echo '<table border="1">';
             echo '<tr>';
@@ -196,11 +331,13 @@
              echo "</tr>";
             }
             echo '</table>';
-          } 
-		  
-		  echo "Edit complete.";
-		  
-		  /*else{
+          } else if ($editmar===true && $db1query===true && $db2query===true){
+            echo "Marinduque edit complete.";
+          } else if($editpal===true && $db1query===true && $db2query===true){
+            echo "Palawan edit complete.";
+          } else throw new Exception("query has not executed perfectly");
+      
+      /*else{
             if($db1query===true){
               $db1query = mysqli_commit($con1);
               if($db1query===false)
@@ -215,14 +352,36 @@
             else throw new Exception("query has not executed perfectly");
           }*/
 
-      } catch (Exception $e){
-        //rollback
-        echo "No changes have been made due to loss of connection between the communicating databases";
+    } catch(Exception $e){
+      echo $e->getMessage();
+      if($e->getMessage()==='query has not executed perfectly'){
         mysqli_rollback($con1);
         if(($paldetect===true || $mardetect===true)&&$comdetect===true)
           mysqli_rollback($con2);
-        exit;
       }
+      echo '<br>Please try again.<a href="home.php">Back</a>';
+    }
+
+    
+
+      // print_r($_SESSION);
+
+      // try{
+          //read write, if you want to read only replace it with READ_ONLY
+          /* if($sql!=='v'){
+            mysqli_begin_transaction($con1, MYSQLI_TRANS_START_READ_WRITE);
+            if(($paldetect===true || $mardetect===true)&&$comdetect===true && $sql!=='v')
+              mysqli_begin_transaction($con2, MYSQLI_TRANS_START_READ_WRITE);
+          } */
+		  
+		  
+
+      // } catch (Exception $e){
+        //rollback
+        // echo "No changes have been made due to loss of connection between the communicating databases";
+        // echo $e->getMessage();
+        // exit;
+      // }
 
    ?>
   </div>
